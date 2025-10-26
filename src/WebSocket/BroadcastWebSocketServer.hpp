edfb16e95f9a697f1b4759c2c54e2f9557d15d96
Kaito Udagawa <umireon@kaito.tokyo>
2025-10-27 04:21:32 +0900

    Update BroadcastWebSocketServer.hpp

--- src/WebSocket/BroadcastWebSocketServer.hpp
+++ src/WebSocket/BroadcastWebSocketServer.hpp
@@ -2,26 +2,35 @@
 WebSocket
 Copyright (C) 2025 Kaito Udagawa umireon@kaito.tokyo
 
-(中略 - ライセンスヘッダー / License header omitted for brevity)
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 #pragma once
 
-#include <string>
-#include <thread>
-#include <mutex>
 #include <atomic>
 #include <condition_variable> // For startup synchronization
+#include <future>         // For std::promise, std::future
+#include <mutex>
 #include <string_view>
+#include <string>
+#include <thread>
+
+#include <libusockets.h> // Include libusockets for us_listen_socket_close
 #include <uwebsockets/App.h>
 #include <uwebsockets/Loop.h> // For uWS::Loop::defer
-#include <future>         // For std::promise, std::future
 
-// Include your logger interface header
-#include <ILogger.hpp> // Example: Replace with your actual logger header
-
-// Include libusockets for us_listen_socket_close
-#include <libusockets.h>
+#include <ILogger.hpp>
 
 namespace KaitoTokyo {
 namespace WebSocket {
